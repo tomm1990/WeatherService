ec392617e2aacfa8f6a7b70fb1334456012a6194
tomm1990 <tomg3210@gmail.com>
2016-07-17 14:19:34 +0300

    Unit Test new project

---
+++ WeatherApp/getWeatherDataUnitTest/WeatherWorldTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Tests
+{
+    [TestClass()]
+    public class WeatherWorldTests
+    {
+        [TestMethod()]
+        public void getWeatherDataTest()
+        {
+            Location location = new Location("Paris");
+            Assert.Fail();
+        }
+    }
+}
